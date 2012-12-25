c15ebe5dbf83b28a318b27a67eb0d0d513e00915
John Anthony <johnanthony@lavabit.com>
2012-12-25 08:03:20 +0000

    Sparkle displacement handled

--- Nyancat.hs
+++ Nyancat.hs
@@ -17,25 +17,29 @@ data State = State { cats :: [Cat]
                    , running :: Bool
                    , screen :: Surface
                    , drawArea :: Rect
+                   , background :: Pixel
                    }
              
-
 advanceSparkle :: State -> Sparkle -> Sparkle
 advanceSparkle st sp = sp { sprkPos = newRect
                           , frame = newFrame
                           }
  where oldRect = sprkPos sp
        newRect = oldRect { rectX = rectX oldRect - velocity sp }
-       maxFrame = (length $ sparkleFrames st) - 1
+       maxFrame = length $ sparkleFrames st
        oldFrame = frame sp
        newFrame = (oldFrame + 1) `mod` maxFrame
+
+advanceSparkleList :: State -> [Sparkle] -> [Sparkle]
+advanceSparkleList st spLst = filter (spkOnscreen st) advSpk
+ where advSpk = map (advanceSparkle st) spLst
 
 advanceState :: State -> State
 advanceState st = st { catFrame = (catFrame st + 1) `mod` maxCatFrame
                      , sparkles = filter (spkOnscreen st) newSparkles
                      }
- where maxCatFrame = (length $ catFrames st) - 1
-       newSparkles = map (advanceSparkle st) (sparkles st)
+ where maxCatFrame = (length $ catFrames st)
+       newSparkles = advanceSparkleList st (sparkles st)
 
 applySurface :: Int -> Int -> Surface -> Surface -> IO Bool
 applySurface x y src dst = blitSurface src Nothing dst offset
@@ -92,12 +96,15 @@ mainLoop st = do
   mapM (drawSparkle st) sparkleLst
   mapM (drawCat st) catLst
   Graphics.UI.SDL.flip scr
+  mapM (\r -> fillRect scr (Just r) bg) blankLst
   delay 50
   mainLoop newSt
  where newSt = advanceState st
        catLst = cats st
        sparkleLst = sparkles st
        scr = screen st
+       blankLst = map sprkPos sparkleLst ++ map catPos catLst
+       bg = background st
 
 main :: IO ()
 main = withInit [InitEverything] $ do
@@ -106,12 +113,14 @@ main = withInit [InitEverything] $ do
     let scrArea = surfaceRect scr
     setCaption "nyan! nyan! nyan! nyan!" []
     bgColour <- mapRGB fmt 0x00 0x33 0x66
-    catFr <- mapM loadImage [ "res/default/fg01.png" 
+    catFr <- mapM loadImage [ "res/default/fg00.png" 
+                            , "res/default/fg01.png"
                             , "res/default/fg02.png"
                             , "res/default/fg03.png"
                             , "res/default/fg04.png"
                             ]
-    spkFr <- mapM loadImage [ "res/default/bg01.png"
+    spkFr <- mapM loadImage [ "res/default/bg00.png"
+                            , "res/default/bg01.png"
                             , "res/default/bg02.png"
                             , "res/default/bg03.png"
                             , "res/default/bg04.png"
@@ -126,5 +135,6 @@ main = withInit [InitEverything] $ do
                      , cats = [catSpawn scrArea catArea]
                      , catFrame = 0
                      , sparkles = [sparkleSpawn scrArea spkArea]
+                     , background = bgColour
                      , running = True
                      }
